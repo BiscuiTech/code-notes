80ff4351eb25436b21acd972c6d4773fe54f57fc
Jean-Cédric Huet <jhuet@biscui.tech>
2020-06-19 15:13:59 -0400

    Added PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,26 @@ module.exports = {
         basePath: '/',
         showThemeInfo: false,
         showDescriptionInSidebar: true,
-        logo: '/static/images/Biscui.png'
+        logo: 'static/images/Biscui.png'
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `BiscuiTech's Code Notes`,
+        short_name: `CodeNotes`,
+        description: `Notes on code. My memory bank.`,
+        start_url: `/`,
+        background_color: `hsl(210, 38%, 95%)`,
+        theme_color: `hsl(345, 100%, 69%)`,
+        display: `standalone`,
+        icon: `static/images/Biscui.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/*`, `/tag/*`],
       },
     },
   ],
